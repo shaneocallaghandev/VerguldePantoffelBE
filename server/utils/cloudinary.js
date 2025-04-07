cd4638853256128e2fd39e7da940f6b96c636411
shaneallmighty <shaneokallahan@hotmail.com>
2025-04-07 20:39:08 +0200

    final changes

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -6,10 +6,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("Cloudinary Config Debug:", {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+  
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
