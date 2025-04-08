c163759a18c04dda33f50433ef39dcd8bab63e74
shaneallmighty <shaneokallahan@hotmail.com>
2025-04-08 10:48:06 +0200

    Clear sensitive log

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -6,13 +6,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("Cloudinary Config Debug:", {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
-
-  
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
