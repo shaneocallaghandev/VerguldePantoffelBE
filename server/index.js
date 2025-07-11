687663542279b2fb74311213a948237677e31f7e
shaneallmighty <shaneokallahan@hotmail.com>
2025-07-11 18:20:57 +0200

    Login endpoint

--- server/index.js
+++ server/index.js
@@ -7,11 +7,25 @@ import cors from "cors";
 import itemRoutes from "./routes/items.js";
 
 const MONGO_URI = process.env.MONGODB_URI;
+const USERNAME = process.env.ADMIN_USERNAME
+const PASSWORD = process.env.ADMIN_PASSWORD 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Login endpoint
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === USERNAME && password === PASSWORD) {
+    return res.status(200).json({ success: true, message: "Login successful" });
+  } else {
+    return res.status(401).json({ success: false, message: "Invalid credentials" });
+  }
+});
+
+// Item routes
 app.use("/api/items", itemRoutes);
 
 mongoose
